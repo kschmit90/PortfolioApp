d876d809056a3355a6205734ee4eb8329ebc7b26
“Kyle <kschmit90@gmail.com>
2014-09-12 18:19:51 -0500

    Create new rails app, initial db create/migrate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortfolioApp::Application.config.secret_token = '9469bf4fe3a8a2b7e197f8b7d10e770043fe3d2cf6ca43111a46a041c757e99e93062ccba9e1159a0de9937ffcf454a43118b4ebf8a016ac882ead20716573e2'
