eefcba735325ebbce090e4a36e35208c3c2e03ff
“Kyle <kschmit90@gmail.com>
2014-09-13 14:38:20 -0500

    validations project and user model, test project model, project index view.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  skip_before_filter :authorize, :only => [:index, :show]
+  
   def index
     @projects = Project.all
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
-  
+  validates :email, :password, :password_confirmation, presence: true
+  validates :email, uniqueness: true
   has_secure_password
 end
