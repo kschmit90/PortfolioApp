cd6809cb65f5bfe416c0a38122897c9939ec26b7
“Kyle <kschmit90@gmail.com>
2014-09-13 15:08:49 -0500

    removed featured from validation, edit project view, links to projects index view

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
   attr_accessible :description, :featured, :github, :name, :other_url, :summary
-  validates :description, :featured, :github, :name, :summary, presence: true
+  validates :description, :github, :name, :summary, presence: true
   validates :name, :github, uniqueness: true
 end
