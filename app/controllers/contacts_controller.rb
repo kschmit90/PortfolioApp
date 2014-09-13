7c506067994108df044e6a93a9a877334e282bf2
“Kyle <kschmit90@gmail.com>
2014-09-13 16:06:08 -0500

    edit, update contact, cleaned up views

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -20,11 +20,20 @@ class ContactsController < ApplicationController
   end
   
   def show
+    @contact = Contact.find(params[:id])
   end
   
   def edit
+    @contact = Contact.find(params[:id])
   end
   
   def update
+    @contact = Contact.find(params[:id])
+  
+    if @contact.update_attributes(params[:contact])
+      redirect_to contact_path(@contact.id)
+    else
+      render "edit"
+    end
   end
 end
