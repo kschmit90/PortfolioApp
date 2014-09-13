e739f27055ee764620b0224914721aeb92eb52d2
“Kyle <kschmit90@gmail.com>
2014-09-12 19:36:04 -0500

    projects routes, link to projects on root

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,22 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+  
+  def new
+    @project = Project.new
+  end
+  
+  def create
+    @project = Project.new(params[:project])
+    if @project.save
+      redirect_to project_path
+    else
+      render "new"
+    end
+  end
+  
+  def show
+    @project = Project.find(params[:id])
+  end
 end
