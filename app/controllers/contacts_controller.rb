04493aa275e302096911785ccfe818c9c386964d
“Kyle <kschmit90@gmail.com>
2014-09-13 15:45:29 -0500

    contact model validations, controller scaffold

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,21 @@
 class ContactsController < ApplicationController
+  skip_before_filter :authorize, :only => [:new]
+  
+  def index
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ActiveRecord::Base
   attr_accessible :email, :message, :name, :phone, :subject, :website
+  validates :email, :message, :name, :subject, presence: true
+  validates :email, uniqueness: true
 end
